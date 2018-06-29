72f2df91e162d13bd131f040ff5752c8e8b748e4
madhusudhanrao beesetty <madhusudhan@madhusudhan-2.local>
2018-06-29 16:31:08 -0700

    rendering api data into component

--- weather-application/src/App.js
+++ weather-application/src/App.js
@@ -6,7 +6,16 @@ import Location from "./components/Location"
 const API_KEY = "bb5d6ae545e675fae9a1230b7f4fcd98"
 
 class App extends React.Component {
-
+  state = {
+    city: undefined,
+    country: undefined,
+    temperature: undefined,
+    humidity: undefined,
+    windspeed: undefined,
+    weatherType: undefined,
+    dayMax : undefined,
+    dayMin : undefined
+  }
   weather = async (e) => {
     e.preventDefault();
     const city = e.target.elements.city.value;
@@ -14,12 +23,31 @@ class App extends React.Component {
     const connect_api = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
     const data = await connect_api.json();
     console.log(data);
+    this.setState({
+      city: data.name,
+      country: data.sys.country,
+      temperature: data.main.temp,
+      humidity: data.main.humidity,
+      windspeed: data.wind.speed,
+      weatherType: data.weather[0].main,
+      dayMax: data.main.temp_max,
+      dayMin: data.main.temp_min
+    })
   }
   render() {
     return (
       <div >
         <Location weather={this.weather} />
-        <WeatherDisplay />
+        <WeatherDisplay 
+          city={this.state.city}
+          country={this.state.country}
+          temperature={this.state.temperature}
+          dayMax={this.state.dayMax}
+          dayMin={this.state.dayMin}
+          humidity={this.state.humidity}
+          windspeed={this.state.windspeed}
+          weatherType={this.state.weatherType}
+        />
       </div>
     );
   }
--- weather-application/src/components/WeatherDisplay.js
+++ weather-application/src/components/WeatherDisplay.js
@@ -4,10 +4,13 @@ class WeatherDisplay extends React.Component {
   render() {
     return(
       <div>
-        <p>Temperature</p>
-        <p>Humidity</p>
-        <p>Wind</p>
-        <p>Conditions</p>
+        <p> City {this.props.city}</p>
+        <p> Country {this.props.country}</p>
+        <p> Today the weather will be like mostly {this.props.weatherType}</p>
+        <p> temperature {this.props.temperature}</p>
+        <p> Day Max {this.props.dayMax} ---- Day Min {this.props.dayMin}</p>
+        <p> Humidity {this.props.humidity}</p>
+        <p> Wind Speed {this.props.windspeed}</p>
       </div>
     )
   }
