e3e31d632368fa2c1ad797143acede5a2f14dbd3
madhusudhanrao beesetty <madhusudhan@madhusudhan-2.local>
2018-06-29 14:24:04 -0700

    making child components and connecting them to APP

---
+++ weather-application/src/components/Location.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Location extends React.Component {
+  render(){
+    return(
+      <div>
+        <p>clicd component to enter location </p>
+      </div>
+    )
+  }
+
+}
+
+export default Location;
---
+++ weather-application/src/components/WeatherDisplay.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class WeatherDisplay extends React.Component {
+  render() {
+    return(
+      <div>
+        <p>display of final weather output</p>
+      </div>
+    )
+  }
+}
+
+export default WeatherDisplay;
